b987bcf01249ea2e394a106b05f926271fad47c2
minsi yang <yangminsi@gmail.com>
2020-05-13 18:02:31 +0100

    Added test for step 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,15 @@ require 'docking_station'
 describe DockingStation do
   subject(:station) {DockingStation.new}
   subject(:bike) {DockingStation.new.release_bike}
+  subject(:park) {DockingStation.new.dock(bike)}
+
+  it "respond to dock method" do
+    expect(station).to respond_to(:dock)
+  end
+
+  it "store the bike" do
+    expect(park).to eq(bike)
+  end
 
   it "responds to release_bike method" do
     expect(station).to respond_to(:release_bike)
