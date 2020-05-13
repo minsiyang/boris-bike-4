58bfc6dfe7791dcb31ea6acb3fb3adabc3ed4b30
J Perrin <perrinjack96@gmail.com>
2020-05-13 16:51:05 +0100

    bike working? test added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,18 @@ require 'docking_station'
 
 describe DockingStation do
   subject(:station) {DockingStation.new}
+  subject(:bike) {DockingStation.new.release_bike}
 
-  it { is_expected.to respond_to(:release_bike) }
+  it "responds to release_bike method" do
+    expect(station).to respond_to(:release_bike)
+  end
 
   it "it is an instance of the bike class" do
-    expect(station.release_bike).to be_instance_of(Bike)
+    expect(bike).to be_instance_of(Bike)
   end
+
+  it " bike to be working? " do
+    expect(bike.working?).to be true
+  end
+
 end
