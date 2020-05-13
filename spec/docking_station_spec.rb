b8f9122a8727e5b1640a47b9aaa2377e7270d754
minsi yang <yangminsi@gmail.com>
2020-05-13 15:34:58 +0100

    Added test to DockingStation_spec for 'get a bike'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
+  subject(:station) {DockingStation.new}
+
   it { is_expected.to respond_to(:release_bike) }
+
+  it "it is an instance of the bike class" do
+    expect(station.release_bike).to be_instance_of(Bike)
+  end
 end
