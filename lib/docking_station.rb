856220baefee49454cccf4485415f165f1ccc331
J Perrin <perrinjack96@gmail.com>
2020-05-13 18:34:39 +0100

    reached attr_reader point with user stories

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,19 @@
 require_relative "bike"
 
 class DockingStation
+attr_reader :bike
 
     def release_bike
       bike = Bike.new
     end
-    
+
+    def dock(bike)
+        @bike = bike
+        @bike
+    end
 
 end
+
+
+#p docking_station = DockingStation.new
+#p docking_station.bike
