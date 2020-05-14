fde02bd171b25236d246ebb3f59118fb3698b043
minsi yang <yangminsi@gmail.com>
2020-05-14 15:58:24 +0100

    reverse back to step 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,10 @@
-require_relative "bike"
+# require_relative "bike"
 
 class DockingStation
 attr_reader :bike
 
     def release_bike
-      @bike = Bike.new
-    end
-
-    def dock(bike)
-        @bike
+      bike
     end
 
 end
-
-
-#p docking_station = DockingStation.new
-#p docking_station.bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   subject(:station) {DockingStation.new}
-  subject(:bike) {Bike.new}
+
 
   it "responds to release_bike method" do
     expect(station).to respond_to(:release_bike)
@@ -11,28 +11,5 @@ describe DockingStation do
   it "release working bikes" do
     expect(station.release_bike).to eq(bike)
   end
-
-  it "it is an instance of the bike class" do
-    expect(bike).to be_instance_of(Bike)
-  end
-
-  it "bike to be working?" do
-    expect(bike.working?).to be true
-  end
-
-  it "respond to dock method" do
-    expect(station).to respond_to(:dock)
-  end
-
-  #it "store the bike" do
-   # station.dock(bike)
-   # expect(park).to eq(bike)
-  #end
-
-  it "store the bike" do
-    station.dock(bike)
-    expect(station.bike).to eq(bike)
-  end
-
 
 end
