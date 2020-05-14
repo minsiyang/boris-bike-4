46ec9aa4a3ef38b21d9da7e1f5f6eb30d42f2a2e
minsi yang <yangminsi@gmail.com>
2020-05-14 15:06:40 +0100

    Added test for release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,11 @@ class DockingStation
 attr_reader :bike
 
     def release_bike
-      bike = Bike.new
+      @bike = Bike.new
     end
 
     def dock(bike)
-        @bike = bike
-        #@bike
+        @bike
     end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,13 @@ require 'docking_station'
 describe DockingStation do
   subject(:station) {DockingStation.new}
   subject(:bike) {Bike.new}
-  #subject(:bike) {DockingStation.new.release_bike}
-  #subject(:park) {DockingStation.new.dock(bike)}
 
   it "responds to release_bike method" do
     expect(station).to respond_to(:release_bike)
+  end
+
+  it "release working bikes" do
+    expect(station.release_bike).to eq(bike)
   end
 
   it "it is an instance of the bike class" do
