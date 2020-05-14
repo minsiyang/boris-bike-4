8fbafcb2bfe0e34f70596d46c88f4d83f6d3b6fd
minsi yang <yangminsi@gmail.com>
2020-05-14 19:20:41 +0100

    Added test to show returned dock bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,6 @@ require 'docking_station'
 describe DockingStation do
   subject(:station) {DockingStation.new}
 
-
   it "responds to release_bike method" do
     expect(station).to respond_to(:release_bike)
   end
@@ -17,4 +16,18 @@ describe DockingStation do
     expect(station).to respond_to(:dock).with(1).argument
   end
 
+  it "when pass bike it returns bike" do
+    bike = Bike.new
+    expect(station.dock(bike)).to eq bike
+  end
+
+  it "respond to bike method" do
+    expect(station).to respond_to(:bike)
+  end
+
+  it "returns docked bike" do
+    bike = Bike.new
+    station.dock(bike)
+    expect(station.bike).to eq bike
+  end
 end
