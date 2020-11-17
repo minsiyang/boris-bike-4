20aa19ae5e2ed544e08df2ce19e13fe9ea6534d4
Minsi Yang <yangminsi@gmail.com>
2020-11-17 21:17:39 +0000

    Refactored DockingStation class and tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,22 +1,7 @@
 require_relative "bike"
-# As a member of the public,
-# So that I reduce the chance of getting a broken bike in future,
-# I'd like to report a bike as broken when I return it.
-# ```
-
-# ```
-# As a maintainer of the system,
-# So that I can manage broken bikes and not disappoint users,
-# I'd like docking stations not to release broken bikes.
-# ```
-
-# ```
-# As a maintainer of the system,
-# So that I can manage broken bikes and not disappoint users,
-# I'd like docking stations to accept returning bikes (broken or not).
 
 class DockingStation
-  attr_reader :bikes, :capacity
+  attr_reader :capacity
 
   DEFAULT_CAPACITY = 20
 
@@ -38,6 +23,8 @@ class DockingStation
   
   private
 
+  attr_reader :bikes
+
   def empty?
     bikes.empty?
   end
@@ -49,39 +36,4 @@ class DockingStation
   def broken?
     bikes.last.working? == false
   end
-#   DEFAULT_CAPACITY = 20
-
-#   attr_accessor :capacity
-
-#   def initialize(capacity=DEFAULT_CAPACITY)
-#     @capacity = capacity
-#     @bikes = []
-#   end
-
-#   def release_bike
-#     raise "No bike available" if empty?
-#     # raise "No bike available" if broke?
-#     bikes.pop
-#   end
-
-#   def dock(bike)
-#     fail "Station is full" if full?
-#     bikes << bike
-#   end
- 
-# private
-
-#   attr_reader :bikes
-
-#   def full?
-#     bikes.count >= capacity
-#   end
-
-#   def empty?
-#     bikes.empty?
-#   end
-
-#   # def broke?
-#   #   bikes.last.broken?
-#   # end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe DockingStation do
 
   it "can't dock a bike passed the default capacity" do
     station = DockingStation.new
-    station.capacity.times { station.dock(working_bike) }
+    DockingStation::DEFAULT_CAPACITY.times { station.dock(working_bike) }
     expect { station.dock(working_bike) }.to raise_error("Docking Station is FULL!")
   end
 
@@ -34,65 +34,5 @@ RSpec.describe DockingStation do
     station.capacity.times { station.dock(working_bike) }
     expect {station.dock(working_bike) }.to raise_error("Docking Station is FULL!")
   end
-  # describe 'initialize' do
-  #   let(:bike) { double(:bike) }
 
-  #   it "has a variable capacity" do
-  #     station = DockingStation.new(40)
-  #     40.times { station.dock(bike) }
-  #     expect { station.dock(bike) }.to raise_error "Station is full"    
-  #   end
-    
-  #   it "has defaults capacity" do
-  #     described_class::DEFAULT_CAPACITY.times do
-  #       subject.dock(bike)
-  #     end
-  #     expect { subject.dock(bike) }.to raise_error "Station is full"
-  #   end
-  # end
-  
-  # describe "#release_bike" do
-  # # let(:bike) { double(:bike) }
-  #   it "doesn't release bike" do
-  #     expect { subject.release_bike }.to raise_error "No bike available"
-  #   end
-
-  #   xit "doesn't realease a broken bike" do
-  #     bike = Bike.new
-  #     bike.report_broken
-  #     subject.dock(bike)
-  #     expect {subject.release_bike }.to raise_error "No bike available"
-  #   end
-
-  #   it "release a working bike" do
-  #     bike = double(:bike)
-  #     allow(bike).to receive(:working?).and_return true
-  #     subject.dock(bike)
-  #     release_bike = subject.release_bike
-  #     expect(release_bike).to be_working
-  #   end
-  # end
-
-  # describe "#dock" do
-  #   it "docks a bike" do
-  #     bike = double(:bike)
-  #     expect(subject.dock(bike)).to eq [bike]
-  #   end
-
-  #   it "docks a broken bike" do
-  #     bike = double(:bike)
-  #     allow(bike).to receive(:report_broken).and_return true
-  #     bike.report_broken
-  #     expect(subject.dock(bike)).to eq [bike]
-  #   end
-
-  #   it "raise an error when the station is full" do
-  #     subject.capacity.times { subject.dock(double(:bike)) }
-  #     expect { subject.dock(double(:bike)) }.to raise_error "Station is full"
-  #   end
-  # end
-
-  # it "has a default capacity" do
-  #   expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-  # end
 end
